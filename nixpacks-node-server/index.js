991dd33b24ed2ce307683a068dc3bd6c5fb83a66
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-08-09 22:07:36 +0200

    Add dummy env var route

--- nixpacks-node-server/index.js
+++ nixpacks-node-server/index.js
@@ -6,6 +6,15 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/env', (req, res) => {
+  const dummyEnvVar = process.env.DUMMY_ENV_VAR
+  if(!dummyEnvVar) {
+    res.status(500).send('DUMMY_ENV_VAR not set')
+  }
+
+  res.send(`DUMMY_ENV_VAR: ${dummyEnvVar}`)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
